b7b7c9342c832f25f9552377dd779bc8f57b882d
Shubham Prakash Jha <prakash.shubhamjha@gmail.com>
2019-07-13 02:40:50 +0530

    Create messages Schema for mongo DB

---
+++ messages.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+    name: String,
+    email: String,
+    query: String
+}, {
+        timestamps: true
+    });
+
+module.exports = mongoose.model('Message', Message);
