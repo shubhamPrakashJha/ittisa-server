aed9b5de49cf705c7fc444822774b2c8b11c602b
Shubham Prakash Jha <prakash.shubhamjha@gmail.com>
2019-07-13 02:29:16 +0530

    Connect MongoDB to Express App

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 // Set the PORT to available port 
 const PORT = 4000;
@@ -13,6 +14,17 @@ const app = express();
 app.use(cors());
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());
+
+
+// connect mongoDB 
+mongoose.connect('mongodb://127.0.0.1:27017/messages', {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
 
 
 //  Express server listening on port 4000
