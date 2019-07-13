df2df5f4ded7d947b9994111a78f0e515ad32b62
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2019-07-13 18:17:09 +0530

    db change

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 
 
 // connect mongoDB 
-mongoose.connect('mongodb://prk54:9825643821P@ds123929.mlab.com:23929/shubham-test/messages', {
+mongoose.connect('mongodb://shubham:shubham123@ds123929.mlab.com:23929/shubham-test', {
     useNewUrlParser: true
 }).then(() => {
     console.log("Successfully connected to the database");
